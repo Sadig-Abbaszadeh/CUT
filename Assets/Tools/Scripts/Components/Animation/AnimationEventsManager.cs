a9ac5af9c1bb5bc586279ad08a380e8b789f92e8
Sadig-Abbaszadeh <abbaszadesadiq@gmail.com>
2021-02-05 10:10:29 +0400

    added anim event callbacks manager

---
+++ Assets/Tools/Scripts/Components/Animation/AnimationEventsManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimationEventsManager : MonoBehaviour
+{
+    [SerializeField]
+    int eventCount;
+    private Action[] stateEndEvents;
+
+    private void Awake()
+    {
+        stateEndEvents = new Action[eventCount];
+    }
+
+    public void Subscribe(int stateIndex, Action action) => stateEndEvents[stateIndex] += action;
+
+    public void Broadcast(int stateIndex) => stateEndEvents[stateIndex]?.Invoke();
+}
