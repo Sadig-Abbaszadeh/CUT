041f783ce8bfe4e4e730afb6e6861a76db4ca4e7
Sadig-Abbaszadeh <abbaszadesadiq@gmail.com>
2021-02-05 09:59:21 +0400

    Added save system, read-only attribute, build debug tools

---
+++ Assets/Tools/Scripts/Components/UI/BuildDebug.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BuildDebug : MonoBehaviour
+{
+    private static BuildDebug Instance;
+
+    [SerializeField]
+    private TextMeshProUGUI debugTextObject;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+    }
+
+    private void LogMessage(string message) => debugTextObject.text += (message + '\n');
+
+    public void ClearText() => debugTextObject.text = "";
+
+    public void Close() => gameObject.SetActive(false);
+
+    public void Open() => gameObject.SetActive(true);
+
+    public static void Log(string message)
+    {
+        if(Instance == null)
+        {
+            Instance = (Instantiate(Resources.Load("BuildDebugCanvas")) as GameObject).transform.GetChild(1).GetComponent<BuildDebug>();
+        }
+
+        Instance.LogMessage(message);
+    }
+
+    public static void Log(object obj) => Log(obj.ToString());
+
+    public static void Clear()
+    {
+        Instance?.ClearText();
+    }
+}
---
+++ Assets/Tools/Scripts/Misc/SaveSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public static class SaveSystem
+{
+    public static void SaveObject(string fileName, object saveObject)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream stream = new FileStream(Path.Combine(Application.persistentDataPath, fileName), FileMode.Create);
+
+        formatter.Serialize(stream, saveObject);
+        stream.Close();
+    }
+
+    public static object LoadObject(string fileName)
+    {
+        string fullPath = Path.Combine(Application.persistentDataPath, fileName);
+
+        if (File.Exists(fullPath))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(fullPath, FileMode.Open);
+
+            object obj = formatter.Deserialize(stream);
+            stream.Close();
+            return obj;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public static void DeleteFile(string fileName)
+    {
+        string fullPath = Path.Combine(Application.persistentDataPath, fileName);
+
+        if (File.Exists(fullPath))
+            File.Delete(fullPath);
+    }
+}
